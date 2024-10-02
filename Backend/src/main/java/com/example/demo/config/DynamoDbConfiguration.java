8b4c8a6de195b6db8ccd5b59ce261a33f5f094fa
AyushSingh0407 <114868218+AyushSingh0407@users.noreply.github.com>
2024-10-02 21:17:05 +0530

    added .env

--- Backend/src/main/java/com/example/demo/config/DynamoDbConfiguration.java
+++ Backend/src/main/java/com/example/demo/config/DynamoDbConfiguration.java
@@ -6,11 +6,14 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDbConfiguration {
+
+    private final Dotenv dotenv = Dotenv.load(); // Load the .env file
 
     @Bean
     public DynamoDBMapper dynamoDBMapper(){
@@ -22,15 +25,15 @@ public class DynamoDbConfiguration {
                 .standard()
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(
-                                "dynamodb.ap-south-1.amazonaws.com",
-                                "ap-south-1"
+                                dotenv.get("AWS_DYNAMODB_ENDPOINT"),
+                                dotenv.get("AWS_REGION")
                         )
                 )
                 .withCredentials(
                         new AWSStaticCredentialsProvider(
                                 new BasicAWSCredentials(
-                                        "AKIAUZPNLERTYLZ6WKXR",
-                                        "A13r5VElLWAzcY2F2PMlNKrmvvUTIv+Q5Cz565/0"
+                                        dotenv.get("AWS_ACCESS_KEY_ID"),
+                                        dotenv.get("AWS_SECRET_ACCESS_KEY")
                                 )
                         )
                 )
--- Backend/src/main/java/com/example/demo/model/DSW.java
+++ Backend/src/main/java/com/example/demo/model/DSW.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,8 +13,16 @@ import lombok.NoArgsConstructor;
 @DynamoDBTable(tableName = "DSW")
 public class DSW {
 
+    @DynamoDBHashKey(attributeName = "dswCollegeName")
     private String dswCollegeName;
+
+    @DynamoDBAttribute(attributeName = "dswCollegeEmail")
     private String dswCollegeEmail;
+
+    @DynamoDBAttribute(attributeName = "dswMobileNo")
     private String dswMobileNo;
+
+    @DynamoDBAttribute(attributeName = "dswPassword")
     private String dswPassword;
+
 }
